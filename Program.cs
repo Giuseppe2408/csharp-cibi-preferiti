69b8ded0be49212e980ef79b231d4222a7feabc4
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-10-27 10:30:01 +0200

    Fine esercizio con bonus

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string[] cibiPreferiti = { "Pasta", "Pizza", "Torta", "Kebab", "McDonald" };
+
+Console.WriteLine("lunghezza classifica è:" + cibiPreferiti.Length);
+
+for(int i = 0; i < cibiPreferiti.Length; i++)
+
+{
+    int index = i + 1;
+    Console.WriteLine("classifica cibi preferiti: " + index + cibiPreferiti[i]);
+}
+
+Console.WriteLine("il mio cibo preferito è " + cibiPreferiti[0]);
+Console.WriteLine("il mio ultimo cibo preferito è " + cibiPreferiti[4]);
+Console.WriteLine("il mio ultimo cibo preferito di mezzo è " + cibiPreferiti[2]);
